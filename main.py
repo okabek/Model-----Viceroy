41384ef97123b05d7185192330803041cc77d95d
Okabe_k <100732818+okabek@users.noreply.github.com>
2024-05-13 12:59:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import re 
+import numpy as np 
 import requests
 import datetime
 import sys
 import time
+import math 
+from scipy import stats
 
 msg = open("data.txt", "r").read()
 lines = msg.splitlines()
-datas = []
-k = 5 # number of neighbours to check
-api_key = "exAhOWAEye9oRcIEn3qX"
 
 ##
+# notes: 
+# ppi is significantly better at forecasting cpi than cpi is for ppi 
+
+def hash(x, y):
+    change = x - y 
+    
+    return x * y * change
+        
 
 def type_write(txt, length):
   txt = txt + "\n"
@@ -22,80 +29,92 @@ def type_write(txt, length):
       time.sleep(length)
 
 
-def has_numbers(inputString):
-    return any(char.isdigit() for char in inputString)
+def is_number(inputString):
+    output = ''.join(c for c in inputString if c.isdigit() or c == '-' or c == '.')
+    numeric = False 
+
+    try:
+        output = float(output)
+        numeric = True 
+
+    except: 
+        None 
+
+    
+    return numeric
 
 
-def find_nearest(type, score):
-    nearest_profile = -1; 
-    smallest_difference = 9999999999999
-
-    for i in datas:
-        sample_score = i["change"]
-        abs_diff = abs(score - sample_score)
-        if (abs_diff < smallest_difference and i["type"] == type):
-            nearest_profile = i
-            smallest_difference = abs_diff
+def forecast(name1, name2, r):
+    r = 2
 
 
-    print(nearest_profile)
+def test(name1, name2):
+    data_a = []
+    data_b = []
 
-    date_object = datetime.datetime.strptime(nearest_profile["date"], "%d/%m/%Y")
-    current_date = date_object.strftime("%Y-%m-%d")
-    next_date = (date_object + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    for i in lines:
+        split = i.split()
 
-    url = "https://marketdata.tradermade.com/api/v1/historical?api_key=" + api_key + "&currency=EURUSD&date=" + current_date
-    request = requests.get(url).json()
-    start_price = request["quotes"][0]["close"]
-
-    url = "https://marketdata.tradermade.com/api/v1/historical?api_key=" + api_key + "&currency=EURUSD&date=" + next_date
-    request = requests.get(url).json()
-    end_price = request["quotes"][0]["close"]
-
-    return round((end_price - start_price) / start_price, 5)
+        if (len(split) <= 1 or split[2] != "USD"):
+            continue
 
 
+        type = split[4] 
+        index = 5
+
+        for i2 in range(len(split) - 5): 
+            data = split[index]
+
+            if (is_number(data) and data != "Composite-20"):
+                break
+
+
+            type = type + " " + split[index]
+            index += 1 
+
+
+        if (len(split) < index + 3):
+            continue
+
+
+        actual = split[index]
+        forecast = split[index + 1]
+        previous = split[index + 2]
+
+        actual = float(''.join(c for c in actual if c.isdigit() or c == '-' or c == '.'))
+        forecast = float(''.join(c for c in forecast if c.isdigit() or c == '-' or c == '.'))
+        previous = float(''.join(c for c in previous if c.isdigit() or c == '-' or c == '.'))
+  
+        if (type == name1 and len(data_a) == len(data_b)):
+            data_a.append(hash(actual, previous))
+
+        elif (type == name2 and len(data_a) == len(data_b) + 1):
+            data_b.append(hash(actual, previous))
+
+
+    if (len(data_a) > len(data_b)):
+        data_a.pop()
+
+    elif (len(data_b) > len(data_b)):
+        data_b.pop()
+
+
+    print("\nTesting using a 5% significance level...")
+    
+    r = np.corrcoef(data_a, data_b)[0][1]
+    n = len(data_a)
+    df = n - 2
+    t = r / math.sqrt((1 - r * r) / df)
+    p = 1 - stats.t.cdf(t, df)
+    
+    if (p < 0.05):
+        type_write(name1 + " and " + name2 + " are correlated.", .05)
+        type_write("r = " + str(r), .05)
+    
+    else: 
+        type_write("There is no correlation between X and Y", .05)
+    
 ##
-
-for i in lines:
-    split = i.split()
-
-    if (len(split) <= 1 or split[2] != "USD" or split[3] != "H"):
-        continue;
-
-
-    type = split[4] + " " + split[5] 
-    index = 5
-
-    for i2 in range(len(split) - 5): 
-        if (has_numbers(split[index])):
-            break; 
-
-
-        index += 1 
-
-
-    if (len(split) < index + 3):
-        continue
-
-
-    actual = float(re.sub('\D', '', split[index]))
-    forecast = float(re.sub('\D', '', split[index + 1]))
-    previous = float(re.sub('\D', '', split[index + 2]))
-
-    profile = {
-        "type": type,
-        "date": split[0],
-        "time": split[1],
-        "actual": actual, 
-        "previous": previous, 
-        "change": actual - previous
-    }
-
-    datas.append(profile)
-
-
-#
 
 art = open("art.txt", 'r').readlines()
 
@@ -105,17 +124,12 @@ for i in art:
 
 print("\n\n\n\n")
 type_write("MODEL | VICEROY", .05)
-type_write("-Looking", .05)
-type_write("-Into", .05)
-type_write("-The", .05)
-type_write("-Past", .05)
+type_write("Forecasting the value of indicator [a] based on indicator [b]", .05)
 print("\n\n")
 
-#
+##
 
-type = input("Enter the name of the economic indicator: ")
-actual = float(input("Enter the newly released actual value: "))
-predicted = float(input("Enter the old forecasted value: "))
-forecast = find_nearest(type, actual - predicted)
+type = input("Enter the name of the 'to forecast' indicator: ")
+type2 = input("Enter the name of the 'to compare' indicator: ")
+forecast = test(type2, type)
 
-print("I forecast a change of " + str(-forecast) + "%" + " for the USD")
